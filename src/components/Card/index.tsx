15de96a94ba98d47fda034f9cf8b3e2f1fc64e55
Arthur Machado <arthur.gmr@hotmail.com>
2024-10-30 22:45:54 -0300

    feat: add card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,38 @@
+import { CoffeeImg, Container, Control, Description, Price, Tags, Title } from "./style"
+
+type Props = {
+  coffee: {
+    id: string
+    title: string
+    description: string
+    tags: string[]
+    price: number
+    image: string
+  }
+}
+
+export function Card({ coffee }: Props ) {
+  return(
+    <Container>
+      <CoffeeImg src={coffee.image} alt={coffee.title}/>
+
+      <Tags>
+        {coffee.tags.map((tag) => (
+          <span key={tag}>{tag}</span>
+        ))}
+      </Tags>
+
+      <Title>{coffee.title}</Title>
+
+      <Description>{coffee.description}</Description>
+
+      <Control>
+        <Price>
+          <span>R$</span>
+          <span>{coffee.price.toFixed(2)}</span>
+        </Price>
+        
+      </Control>
+    </Container>
+  )
+}
